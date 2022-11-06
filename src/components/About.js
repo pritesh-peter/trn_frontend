41ed538e19ac558cf1c6a6115b75f7ef9b7e8810
pritesh-peter <pritesh.peter@gmail.com>
2022-11-06 19:11:51 +0545

    creating base page

--- src/components/About.js
+++
@@ -1,7 +0,0 @@
-const About =()=>{
-    return(
-        <h1>This us About</h1>
-    )
-};
-
-export default About;
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import Base from "../components/Base";
+
+const About = () => {
+    return (
+        <Base>
+            <h1>This us About</h1>
+
+        </Base>
+    )
+};
+
+export default About;
