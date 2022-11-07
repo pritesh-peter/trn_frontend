31e35bd83b3f3ae0a3b44a1c1819c0ade2de8445
pritesh-peter <pritesh.peter@gmail.com>
2022-11-07 22:48:51 +0545

    WIP signup page

--- src/components/Base.js
+++ src/components/Base.js
@@ -3,7 +3,7 @@ import CustomNavbar from "./CustomNavbar";
 const Base =({title="Welcome to our website",children}) => {
 
     return (
-        <div className="container-fluid">
+        <div className="container-fluid p-0 m-0">
            <CustomNavbar/>
             {children}
             <h1>This is footer</h1>
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,12 +1,65 @@
+
+import { Card, CardBody, CardHeader, Container, FormGroup, Input, Label,Form, Button } from "reactstrap";
 import Base from "../components/Base";
 
 const Signup = () => {
     return (
         <Base>
-        <div>
-            <h1>This us Signup</h1>
-            <p>Welcome to Signup page</p>
-            </div>
+      <Container>
+        <Card>
+        <CardHeader>
+         Fill your information to register!!
+        </CardHeader>
+        <CardBody>
+            {/* Creating Form */}
+        <Form>
+            {/* Name Field */}
+            <FormGroup>
+                <Label for="name">Enter Name</Label>
+                <Input
+                type="text"
+                placeholder="Enter here"
+                id="name"
+                />
+            </FormGroup>
+             {/* Email Field */}
+              <FormGroup>
+                <Label for="email">Enter Email</Label>
+                <Input
+                type="email"
+                placeholder="Enter here"
+                id="email"
+                />
+            </FormGroup>
+             {/* Password Field */}
+             <FormGroup>
+                <Label for="password">Enter Password</Label>
+                <Input
+                type="password"
+                placeholder="Enter here"
+                id="password "
+                />
+            </FormGroup>
+            {/* About Field */}
+            <FormGroup>
+                <Label for="about">Enter About yourself</Label>
+                <Input
+                type="textarea"
+                placeholder="Enter here"
+                id="about "
+                style={{height:"250px"}}
+                />
+            </FormGroup>
+            <Container className="text-center">
+
+                <Button color="dark">Register</Button>
+                <Button color="secondary" type="reset" className="ms-2">Reset</Button>
+            </Container>
+        </Form>
+
+        </CardBody>
+        </Card>
+      </Container>
         </Base>
     )
 };
