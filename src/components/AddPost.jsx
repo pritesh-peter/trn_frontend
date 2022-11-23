5d5d78572e5a51002677d62f5502c741d44ca7cc
pritesh-peter <pritesh.peter@gmail.com>
2022-11-23 23:31:52 +0545

    implemented joditEditor

--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Button, Card, CardBody, Container, Form, Input, Label, Placeholder } from 'reactstrap'
 import { loadAllCategories } from '../services/category-service'
+import JoditEditor from "jodit-react";
 
 const AddPost = () =>{
 
+    const editor = useRef(null)
+    const [content,setContent]= useState('')
+
+    const [categories,setCategories] = useState([])
     useEffect( 
         ()=>{
 
             loadAllCategories().then((data)=>{
                 console.log(data)
+                setCategories(data)
             }).catch(error=>{
                 console.log(error)
             })
@@ -25,7 +31,7 @@ const AddPost = () =>{
         <Card className='shadow-sm border-0 mt-2'>
 
             <CardBody>
-                <h3>What going in you mind</h3>
+                <h3>What going in you mind ?</h3>
                 <Form>
 
                     <div className='my-3'>
@@ -39,13 +45,19 @@ const AddPost = () =>{
                     </div>
                     <div className='my-3'>
                         <Label for="content">Post Content</Label>
-                        <Input
+                        {/* <Input
                          type="textarea"
                           id="content"
                           placeholder="Enter here"
                           className='rounded-0'
                           style={{height:'300px'}}
-                          />
+                          /> */}
+
+                         <JoditEditor
+                         ref={editor}
+                         value={content}
+                         onChange={newContent=>setContent(newContent)}
+                         />
                     </div>
                     <div className='my-3'>
                         <Label for="category">Post Category</Label>
@@ -54,7 +66,15 @@ const AddPost = () =>{
                           id="category"
                           placeholder="Enter here"
                           className='rounded-0'
-                          />
+                          >
+                            {
+                                categories.map((category)=>(
+                                   <option value={category.categoryId} key={category.categoryId}>
+                                    {category.categoryTitle}
+                                    </option> 
+                                ))
+                            }
+                            </Input>
                     </div>
 
                     <Container className='text-center'>
