e24dca55509286f0a156286e387afd768c3d4f3d
pritesh-peter <pritesh.peter@gmail.com>
2022-12-02 14:55:39 +0545

    WIP comment section

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
-import { Card, CardBody, CardText, Col, Container, Row } from 'reactstrap'
+import { Card, CardBody, CardText, Col, Container, Input, Row } from 'reactstrap'
 import Base from '../components/Base'
 import { BASE_URL } from '../services/helper'
 import { loadPost } from '../services/post-service'
@@ -33,23 +33,57 @@ const PostPage = () => {
 
         <Row>
             <Col md={{size:12}}>
-            <Card className='mt-3'>
+            <Card className='mt-3 ps-2 border-0 shadow-sm'>
                 {
                     (post) && (
                 <CardBody>
                     <CardText>
                         Posted By <b>{post.user.name}</b> on <b>{printDate(post.addedDate)}</b>
                     </CardText>
+                    <CardText>
+                        <span className='text-muted'>{post.category.categoryTitle}</span>
+                    </CardText>
+                    <div className='divider' style={{
+                        width:'100%',
+                        height:'1px',
+                        background:'#e2e2e2'
+                    }}></div>
                     <CardText className='mt-3'>
-                        <h3>{post.title}</h3>    
+                        <h1>{post.title}</h1>    
                     </CardText>  
-                    <div className="image-container mt-3 container text-center" style={{maxWidth:'50%'}}>
+                    <div className="image-container mt-4 shadow" style={{maxWidth:'50%'}}>
                     <img className="img-fluid" src={BASE_URL+'post/image/'+post.imageName} alt="" />   
-                    </div> 
+                    </div>
+                    <CardText className='mt-5' dangerouslySetInnerHTML={{__html:post.content}}></CardText>
                 </CardBody>
   )
 }
             </Card>
+            </Col>
+        </Row>
+        <Row className='my-4'>
+            <Col md={{
+                size:9,
+                offset:1
+            }}>
+                <h3>Comments ({post ? post.comments.length : 0})</h3>
+                {
+                    post && post.comments.map((c,index)=>(
+                       <Card className='mt-4 border-0'>
+                        <CardBody>
+                            <CardText>
+                                {c.content}
+                            </CardText>
+                        </CardBody>
+                       </Card>
+                    ))
+                }
+
+            <Card className='mt-4 border-0'>
+                        <CardBody>
+                        <Input type="textarea" placeholder='Enter comment here'/>
+                        </CardBody>
+                       </Card>  
             </Col>
         </Row>
     </Container>
