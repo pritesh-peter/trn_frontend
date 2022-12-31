0136697f909a5b825ea2f2196c7d20879adb8337
pritesh-peter <pritesh.peter@gmail.com>
2022-12-31 14:43:21 +0545

    implementing categories API

--- src/components/CategorySideMenu.jsx
+++ src/components/CategorySideMenu.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { toast } from 'react-toastify'
 import { ListGroup, ListGroupItem } from 'reactstrap'
+import { loadAllCategories } from '../services/category-service'
 
 function CategorySideMenu() {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(()=>{
+            loadAllCategories().then(data=>{
+                setCategories([...data])
+            })
+    },[]).catch(error => {
+        console.log(error);
+        toast.error("error in loading categories")
+    })
   return (
     <div>
     <ListGroup>
