d5ad1e94cc98139a35f5b7580c3f9324e66c967f
pritesh-peter <pritesh.peter@gmail.com>
2023-01-03 22:29:05 +0545

    WIP delete post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Card, CardBody, CardText } from 'reactstrap'
+import { getCurrentUserDetail, isLoggedIn } from '../auth'
 
-const Post= ({post={title:"This is default title",content:"This is default content"}}) => {
+const Post= ({post={id:-1,title:"This is default title",content:"This is default content"}}) => {
+  
+  const [user, setUser] = useState(null)
+
+  const [login, setLogin] = useState(null)
+
+  useEffect(()=> {
+    setUser(getCurrentUserDetail())
+    setLogin(isLoggedIn())
+  },[])
+  
   return (
     
     <Card className='border-0 shadow-sm mt-3'>
@@ -13,6 +26,9 @@ const Post= ({post={title:"This is default title",content:"This is default conte
         </CardText>
        <div>
         <Link className='btn btn-secondary border-0' to={'/posts/'+post.postId}>Read More</Link>
+        
+        {isLoggedIn && (user && user.id ===post.user.id ? <Button color='danger' className='ms-2'>Delete</Button>:'')}
+        
        </div>
         </CardBody>
     </Card>
